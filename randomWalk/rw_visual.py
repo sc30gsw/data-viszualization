b7be5f2eb7e7c3a4438927df5f09bc46845e1c9b
sc30gsw <haidoushi875@gmail.com>
2022-06-05 10:11:06 +0900

    複数のランダムウォークを作成する

--- randomWalk/rw_visual.py
+++ randomWalk/rw_visual.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+# プログラムが動作している間、新しいランダムウォークを作成し続ける
+while True:
 # ランダムウォークを生成
-rw = RandomWalk()
-rw.fill_walk()
+  rw = RandomWalk()
+  rw.fill_walk()
 
-# ランダムウォークの点を描画する
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+  # ランダムウォークの点を描画する
+  plt.style.use('classic')
+  fig, ax = plt.subplots()
+  ax.scatter(rw.x_values, rw.y_values, s=15)
+  plt.show()
+
+  keep_runnning = input("別のランダムウォークを生成する?(y/n)")
+  if keep_runnning == 'n':
+    break
