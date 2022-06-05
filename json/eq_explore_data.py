51b590ea9f78d106caba731bee5a53924078f8c5
sc30gsw <haidoushi875@gmail.com>
2022-06-06 06:23:41 +0900

    地震情報を取得

--- json/eq_explore_data.py
+++ json/eq_explore_data.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 # データの構造を調査する
 filename = './data/eq_data_1_day_m1.json'
@@ -6,6 +7,28 @@ filename = './data/eq_data_1_day_m1.json'
 with open(filename) as f:
   # jsonを読み込む
   all_eq_data = json.load(f)
+
+# 地震情報の特徴を取得
+all_eq_dicts = all_eq_data['features']
+
+# マグニチュード
+mags: List[float] = []
+# 経度
+lons: List[float] = []
+# 緯度
+lats: List[float] = []
+[
+  (
+    mags.append(eq_dict['properties']['mag']),
+    lons.append(eq_dict['geometry']['coordinates'][0]),
+    lats.append(eq_dict['geometry']['coordinates'][1])  
+  ) 
+  for eq_dict in all_eq_dicts
+]
+
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
 
 readable_file = './data/readable_eq_data.json'
 # ファイルに書き込む
