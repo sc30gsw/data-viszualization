dad99416c1b19ca71be38a998c8ed94d3098767d
sc30gsw <haidoushi875@gmail.com>
2022-06-05 14:21:41 +0900

    サイコロの面の数と回転数を変更した結果

--- plotly/die_visual.py
+++ plotly/die_visual.py
@@ -6,12 +6,12 @@ from die import Die
 
 # D6を作成する
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # サイコロを転がし結果をリストに格納する
 results = []
 
-for roll_num in range(1000):
+for roll_num in range(50_000):
   result = die_1.roll() + die_2.roll()
   results.append(result)
 
@@ -29,6 +29,6 @@ data = [Bar(x=x_value, y=frequencies)]
 
 x_axis_config = {'title': '結果', 'dtick': 1}
 y_axis_config = {'title': '発生した回数'}
-my_layout = Layout(title='2個の6面のサイコロを1000回転がした結果',
+my_layout = Layout(title='6面のサイコロと10面のサイコロを50000回転がした結果',
   xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
