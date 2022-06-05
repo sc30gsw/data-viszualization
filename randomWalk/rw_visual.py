03418b0c0b9544c4076f5ce75f756dabd68ad9a7
sc30gsw <haidoushi875@gmail.com>
2022-06-05 10:14:57 +0900

    カラーマップでランダムウォークの点にグラデーションをつける

--- randomWalk/rw_visual.py
+++ randomWalk/rw_visual.py
@@ -11,7 +11,9 @@ while True:
   # ランダムウォークの点を描画する
   plt.style.use('classic')
   fig, ax = plt.subplots()
-  ax.scatter(rw.x_values, rw.y_values, s=15)
+  point_numbers = range(rw.num_points)
+  ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+  edgecolors='none', s=15)
   plt.show()
 
   keep_runnning = input("別のランダムウォークを生成する?(y/n)")
