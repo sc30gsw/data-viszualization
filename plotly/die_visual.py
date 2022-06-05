ff66ef4dd58880cd8bbe92803834e2b3bdf215a8
sc30gsw <haidoushi875@gmail.com>
2022-06-05 14:20:05 +0900

    2個のサイコロの合計の確率を可視化したグラフの作成

--- plotly/die_visual.py
+++ plotly/die_visual.py
@@ -5,28 +5,30 @@ from die import Die
 
 
 # D6を作成する
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # サイコロを転がし結果をリストに格納する
 results = []
 
 for roll_num in range(1000):
-  result = die.roll()
+  result = die_1.roll() + die_2.roll()
   results.append(result)
 
 # 結果を分析する
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_results = die_1.num_sides + die_2.num_sides
+for value in range(2, max_results + 1):
   frequency = results.count(value)
   frequencies.append(frequency)
 
 # 結果を可視化する
-x_value = list(range(1, die.num_sides + 1))
+x_value = list(range(2, max_results + 1))
 # 棒グラフデータの作成
 data = [Bar(x=x_value, y=frequencies)]
 
-x_axis_config = {'title': '結果'}
+x_axis_config = {'title': '結果', 'dtick': 1}
 y_axis_config = {'title': '発生した回数'}
-my_layout = Layout(title='6面のサイコロを1000回転がした結果',
+my_layout = Layout(title='2個の6面のサイコロを1000回転がした結果',
   xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
