4e51418ac1c49c471b88f429d1c2ead925522d44
sc30gsw <haidoushi875@gmail.com>
2022-06-05 14:31:55 +0900

    リスト内包表記に変更

--- plotly/die_visual.py
+++ plotly/die_visual.py
@@ -10,25 +10,20 @@ die_3 = Die()
 
 # サイコロを転がし結果をリストに格納する
 results = []
-
-for roll_num in range(50_000):
-  result = die_1.roll() + die_2.roll() + die_3.roll()
-  results.append(result)
+[results.append(die_1.roll() + die_2.roll()) for roll_num in range(50_000)]
 
 # 結果を分析する
+max_results = die_1.num_sides + die_2.num_sides
 frequencies = []
-max_results = die_1.num_sides + die_2.num_sides + die_3.num_sides
-for value in range(3, max_results + 1):
-  frequency = results.count(value)
-  frequencies.append(frequency)
+[frequencies.append(results.count(value)) for value in range(1, max_results + 1)]
 
 # 結果を可視化する
-x_value = list(range(3, max_results + 1))
+x_value = list(range(1, max_results + 1))
 # 棒グラフデータの作成
 data = [Bar(x=x_value, y=frequencies)]
 
 x_axis_config = {'title': '結果', 'dtick': 1}
 y_axis_config = {'title': '発生した回数'}
-my_layout = Layout(title='3個の6面サイコロを50000回転がした結果',
+my_layout = Layout(title='2個の6面サイコロを50000回転がした結果',
   xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
