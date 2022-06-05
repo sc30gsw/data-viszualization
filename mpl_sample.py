3edd2eca16138c5f79ab849c9377961003f35c27
sc30gsw <haidoushi875@gmail.com>
2022-06-05 09:10:56 +0900

    グラフのスタイルを変更する

--- mpl_sample.py
+++ mpl_sample.py
@@ -5,6 +5,14 @@ squares = [1, 4, 9, 16, 25]
 # グラフ領域の生成
 fig, ax = plt.subplots()
 # データを描画
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# グラフのタイトルと軸のラベルを設定する
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# メモリのラベルのサイズを設定する
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
