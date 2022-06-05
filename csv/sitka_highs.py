6c02b60cc336159922caeecdd096b30ff92a2a48
sc30gsw <haidoushi875@gmail.com>
2022-06-05 15:59:27 +0900

    CSVファイルの列(項目)を一覧で取得

--- csv/sitka_highs.py
+++ csv/sitka_highs.py
@@ -1,9 +1,12 @@
 import csv
+from typing import Any, List
 
 filename = './data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
   # 読み込みオブジェクトに指定したファイルオブジェクトを渡す
-  reader = csv.reader(f)
+  reader: Any = csv.reader(f)
   # ファイルの最初の行を読み込む
-  header_row = next(reader)
-  print(header_row)
+  header_row: List[str] = next(reader)
+  
+  for index, column_header in enumerate(header_row):
+    print(index, column_header)
