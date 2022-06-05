fb338dbe32f3db5c55e0bee9d4ebffff202d4c11
sc30gsw <haidoushi875@gmail.com>
2022-06-05 09:25:12 +0900

    データを自動的に計算するようにする

--- mpl_sample.py
+++ mpl_sample.py
@@ -6,15 +6,15 @@ input_value = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 # 点を描画する場所を格納したリスト
-x_values = input_value[:]
-y_values = squares[:]
+x_values = range(0, 1001)
+y_values = [x ** 2 for x in x_values]
 
 # グラフのスタイルを設定
 plt.style.use('seaborn')
 
 # グラフ領域の生成
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 # データを描画
 ax.plot(input_value, squares, linewidth=3)
 
@@ -22,6 +22,10 @@ ax.plot(input_value, squares, linewidth=3)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+# 各軸の範囲を設定する
+# axis([x軸の最小値, x軸の最大値, y軸の最小値, y軸の最大値])
+ax.axis([0, 1100, 0, 1100000])
 
 # メモリのラベルのサイズを設定する
 ax.tick_params(axis='both', labelsize=14)
