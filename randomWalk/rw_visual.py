7109439228e673d5be78f84b13635dfa837920e2
sc30gsw <haidoushi875@gmail.com>
2022-06-05 10:18:24 +0900

    開始点と終了点を強調するよう、開始点と終了点を描画する

--- randomWalk/rw_visual.py
+++ randomWalk/rw_visual.py
@@ -11,9 +11,16 @@ while True:
   # ランダムウォークの点を描画する
   plt.style.use('classic')
   fig, ax = plt.subplots()
+
+  # ランダムウォークの点にカラーマップを適用
   point_numbers = range(rw.num_points)
   ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
   edgecolors='none', s=15)
+
+  # 開始点と終了点を強調する
+  ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+  ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+    s=100)
   plt.show()
 
   keep_runnning = input("別のランダムウォークを生成する?(y/n)")
