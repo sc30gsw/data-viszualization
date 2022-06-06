c90825d870c8b90dbf17de8e4d494b64f8c5f52e
sc30gsw <haidoushi875@gmail.com>
2022-06-07 06:16:49 +0900

    APIから取得したデータを棒グラフにて可視化を行う

--- api/python_repos.py
+++ api/python_repos.py
@@ -1,5 +1,8 @@
-from typing import Dict
+from typing import Dict, List
 import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
 
 # API予呼び出しを作成してそのレスポンスを格納する
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -12,18 +15,33 @@ print(f"ステータスコード: {r.status_code}")
 # APIレスポンスを変数に格納する
 response_dict = r.json()
 
-# 結果を処理する
-print(f"全リポジトリ数: {response_dict['total_count']}")
-
 # リポジトリに関する情報を調べる
 repo_dicts = response_dict['items']
-print(f"情報が返されたリポジトリの数: {len(repo_dicts)}")
 
-# 1つ目のリポジトリを調査する
-for repo_dict in repo_dicts:
-  print(repo_dict)
-  print(f"名前: {repo_dict['name']}")
-  print(f"所有者: {repo_dict['owner']['login']}")
-  print(f"スターの数: {repo_dict['stargazers_count']}")
-  print(f"リポジトリURL: {repo_dict['html_url']}")
-  print(f"説明文: {repo_dict['description']}")
+# リポジトリ名とスター数をリストに格納する
+repo_names: List[str] = []
+stars: List[int] = []
+
+[
+  (
+    repo_names.append(repo_dict['name']),
+    stars.append(repo_dict['stargazers_count'])
+  )
+  for repo_dict in repo_dicts
+]
+
+# データの可視化を行う
+data = [{
+  'type': 'bar',
+  'x': repo_names,
+  'y': stars,
+}]
+
+my_layout = {
+  'title': 'GitHubで最も多くのスターがついているPythonプロジェクト',
+  'xaxis': {'title': 'リポジトリ'},
+  'yaxis': {'title': 'スターの数'},
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
