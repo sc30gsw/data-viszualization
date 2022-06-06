53c28df3bd277fb25cd44bbe962bcba865e90297
sc30gsw <haidoushi875@gmail.com>
2022-06-07 06:06:20 +0900

    取得したレスポンスをループで出力する

--- api/python_repos.py
+++ api/python_repos.py
@@ -1,10 +1,10 @@
-from urllib import response
+from typing import Dict
 import requests
 
 # API予呼び出しを作成してそのレスポンスを格納する
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 # GitHubAPIのバージョンを指定
-headers = {'Accept': 'application/vnd.github.v3+json'}
+headers: Dict[str, str] = {'Accept': 'application/vnd.github.v3+json'}
 # APIにリクエストを送信
 r = requests.get(url, headers=headers)
 print(f"ステータスコード: {r.status_code}")
@@ -13,4 +13,17 @@ print(f"ステータスコード: {r.status_code}")
 response_dict = r.json()
 
 # 結果を処理する
-print(response_dict.keys())
+print(f"全リポジトリ数: {response_dict['total_count']}")
+
+# リポジトリに関する情報を調べる
+repo_dicts = response_dict['items']
+print(f"情報が返されたリポジトリの数: {len(repo_dicts)}")
+
+# 1つ目のリポジトリを調査する
+for repo_dict in repo_dicts:
+  print(repo_dict)
+  print(f"名前: {repo_dict['name']}")
+  print(f"所有者: {repo_dict['owner']['login']}")
+  print(f"スターの数: {repo_dict['stargazers_count']}")
+  print(f"リポジトリURL: {repo_dict['html_url']}")
+  print(f"説明文: {repo_dict['description']}")
